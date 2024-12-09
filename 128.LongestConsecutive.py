db4a2576a9af4abac3674cbbb241a55633c76dde
Vasudev Kishor <139860433+VasudevKishor@users.noreply.github.com>
2024-12-09 23:04:16 +0530

    Add files via upload

--- 128.LongestConsecutive.py
+++ 128.LongestConsecutive.py
@@ -1,13 +1,30 @@
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
 class Solution:
-    def maxProfit(self, prices) -> int:
-        min_price = float('inf')
-        max_profit = 0
-        for i in prices:
-            if i<min_price:
-                min_price = i
-            
-            profit = i - min_price
-            if profit>max_profit:
-                max_profit = profit
-            
-        return max_profit
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        # Dummy node to help with merging
+        dummy = ListNode(-1)
+        current = dummy
+
+        # Merge lists while both are not empty
+        while list1 and list2:
+            if list1.val < list2.val:
+                current.next = list1
+                list1 = list1.next
+            else:
+                current.next = list2
+                list2 = list2.next
+            current = current.next
+
+        # Append the remaining nodes of the non-empty list
+        if list1:
+            current.next = list1
+        elif list2:
+            current.next = list2
+
+        # Return the merged list starting from the next node of the dummy
+        return dummy.next
